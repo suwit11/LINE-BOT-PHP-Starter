6327c1e815655014a6ed16cb36913a740dc8e683
suwit11 <suwit_nongkarwee@hotmail.com>
2017-05-06 10:38:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,14 +18,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				{
-            "type":"text",
-            "text":"Hello, user"
-        },
-        {
-            "type":"text",
-            "text":"May I help you?"
-        }
+				'type' => 'text',
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
