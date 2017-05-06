6c4f646d05fffd968ada5220075187cd2b83c946
suwit11 <suwit_nongkarwee@hotmail.com>
2017-05-06 12:51:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,7 @@
 <?php
-$access_token = 'IMP/LG0+BdBFbNWC9vQMAVBZJ9OXOULymEFH2+c9tziXYnMYxAeuol7A82lHCT+3DHAd0zH0X3y70T1CLaqg1ubneNusu/uLDWzTytE5pEc/FiME31SsHMpYlwHJahbF1Dje3wcjjLs+U8QCClO3twdB04t89/1O/w1cDnyilFU=';
-
+ 
+$strAccessToken = "ACCESS_TOKEN";
+ 
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
  
@@ -43,5 +44,3 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
- 
-echo "OK";
