7ba9ca633d9b80dc246e8de5ad1ee8c9630a4546
suwit11 <suwit_nongkarwee@hotmail.com>
2017-05-06 13:02:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'สวัสดี') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => ฉันไม่มีชื่อ
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
