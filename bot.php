52722311334b361cb8a50c063a172a703e85b3d8
suwit11 <suwit_nongkarwee@hotmail.com>
2017-05-06 12:40:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,7 +1,6 @@
 <?php
- 
-$strAccessToken = "IMP/LG0+BdBFbNWC9vQMAVBZJ9OXOULymEFH2+c9tziXYnMYxAeuol7A82lHCT+3DHAd0zH0X3y70T1CLaqg1ubneNusu/uLDWzTytE5pEc/FiME31SsHMpYlwHJahbF1Dje3wcjjLs+U8QCClO3twdB04t89/1O/w1cDnyilFU=";
- 
+$access_token = 'IMP/LG0+BdBFbNWC9vQMAVBZJ9OXOULymEFH2+c9tziXYnMYxAeuol7A82lHCT+3DHAd0zH0X3y70T1CLaqg1ubneNusu/uLDWzTytE5pEc/FiME31SsHMpYlwHJahbF1Dje3wcjjLs+U8QCClO3twdB04t89/1O/w1cDnyilFU=';
+
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
  
@@ -45,4 +44,8 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
  
-?>
+echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
